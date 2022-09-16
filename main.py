b917e7a02c63104e8d0427c50196c673d362e0d4
Zulmechan <111186747+Zulmechan@users.noreply.github.com>
2022-09-16 23:11:41 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+import os
+
+os.system("clear")
+print("[!] Tools dinonaktifkan sementara oleh admin")
+print("[!] Silahkan kunjungi yt admin ZULMI ANGGARA")
+print("[!] Untuk mendapatkan update-an terbaru pastinya..")
+os.system("exit")
